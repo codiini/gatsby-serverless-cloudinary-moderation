782de59216792dedd529ff4c7eff9e8641a8180b
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2022-07-23 17:14:43 +0100

    update

--- src/api/notification.js
+++ src/api/notification.js
@@ -5,14 +5,12 @@ const database = client.db("cloudinary")
 const collection = database.collection("moderated_images")
 
 export default async function handler(req, res) {
-  try {
-    const moderatedImage = req.body
-    const result = await collection.insertOne(moderatedImage)
-    console.log(result.insertedId)
-    res.json({ msg: "Success" })
-  } catch (err) {
-    res.status(500).json({ msg: err.message })
-  } finally {
-    await client.close()
-  }
+    try {
+        const moderatedImage = req.body
+        const result = await collection.insertOne(moderatedImage)
+        console.log(result.insertedId)
+        res.json({ msg: "Success" })
+    } catch (err) {
+        res.status(500).json({ msg: err.message })
+    }
 }
